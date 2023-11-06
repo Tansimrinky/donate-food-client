fae637748871dff92bb03277732ece89899667ee
Tansimrinky <tansimrinkybindu@gmail.com>
2023-11-06 18:10:56 +0600

    banner added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,11 @@
+import Banner from "../../Componenets/Navbar/Banner";
+
 
 
 const Home = () => {
     return (
         <div>
+            <Banner></Banner>
             This is home
         </div>
     );
