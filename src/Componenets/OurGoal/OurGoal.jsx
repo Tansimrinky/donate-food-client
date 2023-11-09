64a223590515732be7224742beb4ff7b2eed7718
Tansimrinky <tansimrinkybindu@gmail.com>
2023-11-09 21:07:38 +0600

    some changes added

--- src/Componenets/OurGoal/OurGoal.jsx
+++ src/Componenets/OurGoal/OurGoal.jsx
@@ -4,8 +4,8 @@ const OurGoal = () => {
     return (
         <div>
             <h2 className="text-5xl font-bold text-center">Our Goal</h2>
-            <div className="flex justify-between ">
-                <div className="w-1/2 my-4">
+            <div className="md:flex  md:justify-between ">
+                <div className="w-1/2 my-4 text-center">
                     <p className="text-xl font-bold text-green-900 my-4">Eradicate Hunger: <span className="font-medium text-slate-700">Strive to eliminate hunger in communities by providing nutritious and wholesome food to those in need.</span></p>
                     <p className="text-xl font-bold text-green-900  my-4">Reduce Food Waste:  <span className="font-medium text-slate-700">Work towards minimizing food waste by redistributing surplus and unused food to individuals and families facing food insecurity.</span></p>
                     <p className="text-xl font-bold text-green-900   my-4">Community Empowerment:  <span className="font-medium text-slate-700"> Empower local communities by fostering a spirit of sharing and support, creating a network that cares for one another.</span></p>
